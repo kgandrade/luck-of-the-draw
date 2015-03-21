bfb5145f12e7a673a6a48297dbce9440d7d218f8
Kevin G. Andrade <kgandrade3@gmail.com>
2015-03-21 15:41:29 -0700

    tweak publish script

--- _publish.sh
+++ _publish.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
+rm -Rf _out
 tarbell generate _out
 cp -R _out/* ~/src/asylum/
 cd ~/src/asylum
-git stash
-git checkout gh-pages
-git add -A .
+git add -A
 git commit -m "updated for publication"
 git push origin gh-pages
